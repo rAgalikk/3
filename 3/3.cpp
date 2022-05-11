a30a421b7ff3fbf1e16cf37bb4dc7d643c5c585a
Миша Миша <donecmisa50@gmail.com>
2022-05-12 00:12:22 +0300

    Добавьте файлы проекта.

---
+++ 3/3.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+int main()
+{
+    int i = 19;
+    int in = 15;
+    int inn = 20;
+    std::cout << "Result i + in + inn = " << i + in + inn << ";" << "\n";
+    float f = 10.10;
+    float fl;
+    float apple = 10.10;
+    float pear = 230;
+    float milk = 30.50;
+    float honey = 550;
+    float potatoe = 30.75;
+    std::cout << "Shopping list: = " << apple + pear + milk + honey + potatoe << ";" << "\n";
+
+
+    int first = 25;
+    int second = 10;
+    std::cout << "first = " << first << ";" << "\n";
+    std::cout << "second = " << second << ";" << "\n";
+    first = first + second;
+    second = first - second;
+    first = first - second;
+    std::cout << "first = " << first << ";" << "\n";
+    std::cout << "second = " << second << ";" << "\n";
+
+
+    int a = 5;
+    int b = 10;
+    std::cout << "a = " << a << ";" << "\n";
+    std::cout << "b = " << b << ";" << "\n";
+    std::swap(a, b);
+    std::cout << "a = " << a << ";" << "\n";
+    std::cout << "b = " << b << ";" << "\n";
+
+
+    std::cout << "5 zadanie" << ";" << "\n";
+    int alahakbar = 2147483647;
+    std::cout << alahakbar + 1 << ";" << "\n";
+
+    std::cout << "6 zadanie" << ";" << "\n";
+    int a1 = 10;
+    int a2 = a1;
+    int a3 = a1;
+    printf("a1 = %d;\na2 = %d;\na3 = %d;\n", a1, a2, a3);
+
+    double d = 666.999;
+    double dou;
+    char c = 'c';
+    char ch;
+    bool boo = true;
+    bool booo;
+}
+
+
